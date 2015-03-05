3b422701185b01c5f5199754ff7bde5fe544a20b
LiQi <liqi1993@hotmail.com>
2015-03-05 11:16:17 +0800

    Parser storage

--- CS2103 project V0.1/src/Parser.java
+++ CS2103 project V0.1/src/Parser.java
@@ -1,4 +1,9 @@
 
 public class Parser {
-
+	//Parser part -- LIQI
+	private static void parse(Cone_Organizer item) {
+		int index = item.command.indexOf(' ');
+		item.command_type = item.command.substring(0,index);
+		item.detail = item.command.substring(index, item.command.length());
+	}
 }
--- CS2103 project V0.1/src/Storage.java
+++ CS2103 project V0.1/src/Storage.java
@@ -1,4 +1,17 @@
 
 public class Storage {
+	//storage part -- Qiao Di
+		private static void displayCommand(Cone_Organizer item) {
+			// TODO Auto-generated method stub
+			
+		}
+	//Storage part -- Qiao Di
+		private static void addCommand(Cone_Organizer item) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
+
 
 }
