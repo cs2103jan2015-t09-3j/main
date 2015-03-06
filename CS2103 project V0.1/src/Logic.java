e5532639d7fdb8f3a7a84ff9e7228931a6828932
Kim <ertty0120@hotmail.com>
2015-03-06 23:49:35 +0800

    the methods in storage should take in and modify the cone_organizer objects, so i made necessary changes to the code. However, the displayCommand method doesn't seem to be working

--- CS2103 project V0.1/src/Logic.java
+++ CS2103 project V0.1/src/Logic.java
@@ -1,32 +1,31 @@
-
 public class Logic {
 	Parser p = new Parser();
 	Storage s = new Storage();
-	
-public void executeCommand(Cone_Organizer cmd) {
-		
+
+	public void executeCommand(Cone_Organizer cmd) {
+
 		p.parse(cmd);
 		implementCommand(cmd);
-		
-		
-	}
-public void implementCommand(Cone_Organizer cmd) {
-	switch (cmd.command_type){
-	case "add" :{
-		s.addCommand(cmd);
-		System.out.println("yay");
-	}
-	case "display" :{
-		s.displayCommand();
-	}
-	
-	}
-	case "delete" :{
-		s.deleteCommand(cmd);
-	}
-	case "edit" :{
-		s.editCommand(cmd);
-	}
+
 	}
 
+	public void implementCommand(Cone_Organizer cmd) {
+		switch (cmd.command_type) {
+		case "add": {
+			s.addCommand(cmd);
+			
+		}
+		case "display": {
+			s.displayCommand();
+		}
+
+		case "delete": {
+			s.deleteCommand(cmd);
+		}
+		case "edit": {
+			s.editCommand(cmd);
+		}
+		}
+
+	}
 }
--- CS2103 project V0.1/src/Storage.java
+++ CS2103 project V0.1/src/Storage.java
@@ -1,22 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 
 public class Storage {
 	
-	public class Storage {
+
 		
 		File file = new File("CO-NE.txt");
 		
 		//Storage part -- QiaoDi
-		public void addCommand(String cmd) {
+		public void addCommand(Cone_Organizer cmd) {
 			// TODO Auto-generated method stub
 			try {
 			         BufferedWriter output = new BufferedWriter(new FileWriter(file));
-			         output.write(cmd);
+			         output.write(cmd.detail);
 			         output.close();
 			     } catch ( IOException e ) {
 			          e.printStackTrace();
 			     }		    
 			
-			System.out.println("\"", cmd, "is added."\"");
+			System.out.println("\""+ cmd.detail+ "is added.\"");
 			}
 			
 	//storage part -- QiaoDi
@@ -26,7 +32,7 @@ public class Storage {
 			System.out.println(file.toString());
 		}	
 
-		}
+		
 		public void deleteCommand(Cone_Organizer cmd){
 			
 		}
