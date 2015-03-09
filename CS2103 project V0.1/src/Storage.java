d862ac64eee64a505e60eac0c5d616478ea2dc31
Kim <ertty0120@hotmail.com>
2015-03-09 13:43:49 +0800

    delete function updated to make change to date as well

--- CS2103 project V0.1/src/Storage.java
+++ CS2103 project V0.1/src/Storage.java
@@ -40,9 +40,10 @@ public class Storage {
 		//this method should delete the item from the list at 'index-1' and push up the list by one index
 		public void deleteCommand(int index, ArrayList<Cone_Organizer> list){
 			
-			System.out.println("deleted from file : \"" + list.get(index).detail
+			System.out.println("deleted from file : \"" + list.get(index-1).detail
 					+ "\"");
-			list.get(index).detail = DEFAULT_STRING;
+			list.get(index-1).detail = DEFAULT_STRING;
+			list.get(index-1).date = DEFAULT_STRING;
 			organizeStorage(list);
 		}
 		private static void organizeStorage(ArrayList<Cone_Organizer> list) {
@@ -50,6 +51,10 @@ public class Storage {
 				if (list.get(i).detail == DEFAULT_STRING && list.get(i+1).detail != DEFAULT_STRING) {
 					list.get(i).detail = list.get(i+1).detail;
 					list.get(i+1).detail = DEFAULT_STRING;
+				}
+				if (list.get(i).date == DEFAULT_STRING && list.get(i+1).date != DEFAULT_STRING) {
+					list.get(i).date = list.get(i+1).date;
+					list.get(i+1).date = DEFAULT_STRING;
 				}
 
 			}
