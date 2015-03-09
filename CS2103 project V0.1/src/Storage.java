da9e4712ddd30f84a14d5e08dc03718a02ae0139
foolkk <kirkchocolate@gmail.com>
2015-03-09 10:21:35 +0800

    display, deleteCmd, deleteFromFile

--- CS2103 project V0.1/src/Storage.java
+++ CS2103 project V0.1/src/Storage.java
@@ -1,54 +1,72 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 
 
 public class Storage {
 	
-
+	File file = new File("CO-NE.txt");
+	
+	public class Storage {
 		
-		File file = new File("CO-NE.txt");
+		//File file = new File("CO-NE.txt");
 		
 		//Storage part -- QiaoDi
 		public void addCommand(Cone_Organizer cmd) {
 			// TODO Auto-generated method stub
+			String cmd = cmd.detail;
 			try {
 			         BufferedWriter output = new BufferedWriter(new FileWriter(file));
-			         output.write(cmd.detail);
+			         output.write(cmd);
 			         output.close();
 			     } catch ( IOException e ) {
 			          e.printStackTrace();
 			     }		    
 			
-			System.out.println("\""+ cmd.detail+ "is added.\"");
-			}
+			System.out.println("\"", cmd, "is added."\"");
+		}
+	
 			
-	//storage part -- QiaoDi
+		//storage part -- QiaoDi
 		public void displayCommand() {
-			// TODO Auto-generated method stub
-			
-			System.out.println(file.toString());
+		// TODO Auto-generated method stub			
+		System.out.println(file.toString());
 		}	
 
-		
-		public void deleteCommand(int index){
-			
+		public void deleteCommand(Cone_Organizer cmd){
+			deleteFromFile(cmd);
 		}
+		
 		public void editCommand(Cone_Organizer cmd){
 			
 		}
-		private static void readToFile(Cone_Organizer cmd){
+		private static void readFromFile(Cone_Organizer cmd){
 			
 		}
 		private static void writeToFile(Cone_Organizer cmd){
 			
 		}
-		private static void deleteFromFile(Cone_Organizer cmd){
-			
-		}
 		
+		private static void deleteFromFile(int taskNum){
+			File tmp = File.createTempFile("tmp", "");
+
+		    BufferedReader br = new BufferedReader(new FileReader(file));
+		    BufferedWriter bw = new BufferedWriter(new FileWriter(tmp));
+
+		    for (int i = 0; i < toRemove; i++)
+		        bw.write(String.format("%s%n", br.readLine()));
+
+		    br.readLine(); //skip the deleted line
+            
+		    String line; 
+		    while(null != (line = br.nextLine()){
+		    	String str = taskNum + line.substring(1);
+		    	bw.write(String.format("%s%n",str));
+		        taskNum ++;
+		    }
+		     
+		    File oldFile = new File(f);
+		    if (oldFile.delete())
+		        tmp.renameTo(oldFile);		    	
+	   
+		}
 
 }
 
