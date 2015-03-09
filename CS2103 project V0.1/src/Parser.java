07166efc3ba815f5ffc72d9be0e49495929dbb5a
LIQI <liqi1993@hotmail.com>
2015-03-09 11:24:33 +0800

    parser date

--- CS2103 project V0.1/src/Parser.java
+++ CS2103 project V0.1/src/Parser.java
@@ -6,7 +6,6 @@ public class Parser {
 		int index = cmd.command.indexOf(' ');
 		cmd.command_type = cmd.command.substring(0,index);
 		cmd.detail = cmd.command.substring(index, cmd.command.length());
-		
 		int index2 = cmd.command.indexOf("");
 		cmd.date = cmd.command.substring(index2, cmd.command.length());
 	}
