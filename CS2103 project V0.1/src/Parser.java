0557a4b3f910664407fd65a7cc2d1fd38127b223
LIQI <liqi1993@hotmail.com>
2015-03-16 12:49:26 +0800

    Paser add function

--- CS2103 project V0.1/src/Parser.java
+++ CS2103 project V0.1/src/Parser.java
@@ -5,23 +5,35 @@ public class Parser {
 	public void parse(Cone_Organizer cmd) {
 		int index = cmd.command.indexOf(' ');
 		if(index!=-1){
-			cmd.command_type = cmd.command.substring(0,index);
-			if(cmd.command.contains("by")){
-				int index2 = cmd.command.indexOf("by");
-				cmd.detail = cmd.command.substring(index+1,index2);
-				cmd.date = cmd.command.substring(index2+3, cmd.command.length());
-				
-			}
-			else if(index!= cmd.command.length()-1){
-				cmd.detail = cmd.command.substring(index+1, cmd.command.length());
-			}
+			identifyType(cmd, index);
+
 		}
 		else{
-			cmd.command_type=cmd.command;
+			cmd.command_type = cmd.command;
+		}	
+		
+	}
+	
+	public void identifyType (Cone_Organizer cmd, index){
+		cmd.command_type = cmd.command.substring(0,index);
+		if(cmd.command.contains("by")){
+			timed(cmd);
 		}
+		else if (index!= cmd.command.length()-1){
+			floating(cmd);
+		}
+	}
+	
+	public void timed(Cone_Organizer cmd){
+	
+			int index2 = cmd.command.indexOf("by");
+			cmd.detail = cmd.command.substring(index+1,index2);
+			cmd.date = cmd.command.substring(index2+3, cmd.command.length());		
+	}
+	
+	public void floating(Cone_Orgainzer cmd){
 		
-		
-		
+			cmd.detail = cmd.command.substring(index+1, cmd.command.length());
 		
 	}
 }
