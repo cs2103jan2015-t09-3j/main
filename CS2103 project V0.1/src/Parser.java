e18daa3c9c756917710e9792811825cb26eac206
Kim <ertty0120@hotmail.com>
2015-03-16 12:52:52 +0800

    updated parser

--- CS2103 project V0.1/src/Parser.java
+++ CS2103 project V0.1/src/Parser.java
@@ -14,24 +14,24 @@ public class Parser {
 		
 	}
 	
-	public void identifyType (Cone_Organizer cmd, index){
+	public void identifyType (Cone_Organizer cmd, int index){
 		cmd.command_type = cmd.command.substring(0,index);
 		if(cmd.command.contains("by")){
-			timed(cmd);
+			timed(cmd, index);
 		}
 		else if (index!= cmd.command.length()-1){
-			floating(cmd);
+			floating(cmd, index);
 		}
 	}
 	
-	public void timed(Cone_Organizer cmd){
+	public void timed(Cone_Organizer cmd, int index){
 	
 			int index2 = cmd.command.indexOf("by");
 			cmd.detail = cmd.command.substring(index+1,index2);
 			cmd.date = cmd.command.substring(index2+3, cmd.command.length());		
 	}
 	
-	public void floating(Cone_Orgainzer cmd){
+	public void floating(Cone_Organizer cmd, int index){
 		
 			cmd.detail = cmd.command.substring(index+1, cmd.command.length());
 		
