ebe4a8b32a331119884fabdf99a4baf0eb401da6
foolkk <kirkchocolate@gmail.com>
2015-03-19 10:32:27 +0800

    tada

--- CS2103 project V0.1/src/Logic.java
+++ CS2103 project V0.1/src/Logic.java
@@ -1,9 +1,4 @@
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 
 
@@ -14,7 +9,6 @@ public class Logic {
 
 
 	public void executeCommand(Cone_Organizer cmd, ArrayList<Cone_Organizer> list) {
-		assert list !=null;
 
 		p.parse(cmd);
 		implementCommand(cmd, list);
@@ -22,35 +16,12 @@ public class Logic {
 	}
 
 	public void implementCommand(Cone_Organizer cmd, ArrayList<Cone_Organizer> list) {
-		Logger logger = Logger.getLogger("TextBuddy");
-		logger.log(Level.INFO,  "going to start processing");
-		FileHandler fh;
-		
 		switch (cmd.command_type) {
 		case "add": {
-			try {
-				fh = new FileHandler("MyLogFile.log");
-		        logger.addHandler(fh);
-		        SimpleFormatter formatter = new SimpleFormatter();  
-		        fh.setFormatter(formatter);  
-		        
-
-		        // the following statement is used to log any messages  
-		        logger.info("My first log"); 
-			} catch (SecurityException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}  
-
-	        logger.info("Hi How r u?");
-	
 			s.addCommand(cmd, list);
-			break;
-			
+			break;			
 		}
+		
 		case "display": {
 			s.displayCommand(list);
 			break;
@@ -62,20 +33,18 @@ public class Logic {
 			s.deleteCommand(index, list);
 			break;
 		}
+		
 		case "edit": {
 			int index;
 			index=Integer.parseInt(cmd.detail);
 			s.editCommand(list, index);
 			break;
 		}
+		
 		case "clear": {
 			s.clearCommand(list);
 			break;
 		}
-
 		}
-
 	}
-
-	
 }
