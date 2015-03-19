ca43f65dfd165b2dca7f5f8a86555c91e53b4657
LIQI <liqi1993@hotmail.com>
2015-03-19 09:56:45 +0800

    logging

--- CS2103 project V0.1/src/Logic.java
+++ CS2103 project V0.1/src/Logic.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 
 
@@ -9,6 +14,7 @@ public class Logic {
 
 
 	public void executeCommand(Cone_Organizer cmd, ArrayList<Cone_Organizer> list) {
+		assert list =null;
 
 		p.parse(cmd);
 		implementCommand(cmd, list);
@@ -16,8 +22,31 @@ public class Logic {
 	}
 
 	public void implementCommand(Cone_Organizer cmd, ArrayList<Cone_Organizer> list) {
+		Logger logger = Logger.getLogger("TextBuddy");
+		logger.log(Level.INFO,  "going to start processing");
+		FileHandler fh;
+		
 		switch (cmd.command_type) {
 		case "add": {
+			try {
+				fh = new FileHandler("MyLogFile.log");
+		        logger.addHandler(fh);
+		        SimpleFormatter formatter = new SimpleFormatter();  
+		        fh.setFormatter(formatter);  
+		        
+
+		        // the following statement is used to log any messages  
+		        logger.info("My first log"); 
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  
+
+	        logger.info("Hi How r u?");
+	
 			s.addCommand(cmd, list);
 			break;
 			
@@ -43,10 +72,7 @@ public class Logic {
 			s.clearCommand(list);
 			break;
 		}
-		default:{
-			throw new AssertionError(cmd.command_type);
 
-		  }
 		}
 
 	}
