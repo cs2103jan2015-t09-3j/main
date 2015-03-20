5b7beda43f2c028763233b2018b075c2e3a19e6c
LIQI <liqi1993@hotmail.com>
2015-03-20 12:37:28 +0800

    Parser API

--- CS2103 project V0.1/src/Parser.java
+++ CS2103 project V0.1/src/Parser.java
@@ -2,6 +2,11 @@
 public class Parser {
 	
 	//Parser part -- LIQI
+	
+	/**
+	* This method will recognize whether the commands are tasks or simply commands
+	* @param cmd  	This object contains just the user's command	 
+	*/
 	public void parse(Cone_Organizer cmd) {
 		int index = cmd.command.indexOf(' ');
 		if(index!=-1){
@@ -13,11 +18,20 @@ public class Parser {
 		}	
 		
 	}
-	
+
+	/**
+	* This method will assign user's command to command type since there is only command
+	* @param cmd  	This object contains command only and no tasks are specified 
+	*/
 	public void commandOnly (Cone_Organizer cmd){
 		cmd.command_type = cmd.command;
 	}
 	
+	/**
+	* This method will recognize whether it is a floating or timed task
+	* @param cmd  	This object contains the task content and date of the new task
+	* @param index	Index separates the command from task details
+	*/
 	public void identifyType (Cone_Organizer cmd, int index){
 		cmd.command_type = cmd.command.substring(0,index);
 		if(cmd.command.contains("by")){
@@ -28,6 +42,11 @@ public class Parser {
 		}
 	}
 	
+	/**
+	* This method will handle timed task and task details
+	* @param cmd  	This object contains the task type, task content and date of the new task
+	* @param index	Index separates the command from task details	 
+	*/
 	public void timed(Cone_Organizer cmd, int index){
 	
 			int index2 = cmd.command.indexOf("by");
@@ -35,6 +54,11 @@ public class Parser {
 			cmd.date = cmd.command.substring(index2+3, cmd.command.length());		
 	}
 	
+	/**
+	* This method will handle floating task and task details
+	* @param cmd  	This object contains the task type and task content of the new task
+	* @param index	Index separates the command from task details	 
+	*/
 	public void floating(Cone_Organizer cmd, int index){
 		
 			cmd.detail = cmd.command.substring(index+1, cmd.command.length());
